5bd355ae6cf0074467490bb6813dfe87e1ae93b3
Derrick Fountain <djfj74@users.noreply.github.com>
2016-04-05 08:52:22 -0400

    Update Form1.vb

--- Glyph/Form1.vb
+++ Glyph/Form1.vb
@@ -2,5 +2,6 @@
 
     Private Sub AdobeButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AdobeButton2.Click
         LoginForm1.Show()
+        ´Xauth Implemented
     End Sub
 End Class
