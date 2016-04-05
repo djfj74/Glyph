0696d6a724dc6a1487ba8e3bcda903297a3ea2f3
Derrick Fountain <djfj74@icloud.com>
2016-04-05 07:39:13 -0400

    no message

--- Glyph/SplashScreen1.Designer.vb
+++ Glyph/SplashScreen1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class SplashScreen1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -28,22 +28,26 @@ Partial Class Form1
         '
         'PictureBox1
         '
-        Me.PictureBox1.Image = Global.Glyph.My.Resources.Resources.glyph_logo_v1
-        Me.PictureBox1.Location = New System.Drawing.Point(-1, 0)
+        Me.PictureBox1.Image = Global.Glyph.My.Resources.Resources.glyph_logo_720x322
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 1)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(182, 240)
+        Me.PictureBox1.Size = New System.Drawing.Size(720, 324)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
-        'Form1
+        'SplashScreen1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(179, 237)
+        Me.ClientSize = New System.Drawing.Size(714, 321)
+        Me.ControlBox = False
         Me.Controls.Add(Me.PictureBox1)
-        Me.Cursor = System.Windows.Forms.Cursors.AppStarting
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "SplashScreen1"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
